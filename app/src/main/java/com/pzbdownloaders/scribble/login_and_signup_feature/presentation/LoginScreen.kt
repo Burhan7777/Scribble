02d7a2a47816a98a64a60c477a752c691190e5ce
Peerzada Burhan Ahmad <peerzadaburhan7@gmail.com>
2024-10-12 11:23:59 +0530

    Adds back button to the login screen

--- app/src/main/java/com/pzbdownloaders/scribble/login_and_signup_feature/presentation/LoginScreen.kt
+++ app/src/main/java/com/pzbdownloaders/scribble/login_and_signup_feature/presentation/LoginScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIos
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.CardDefaults
@@ -90,6 +91,13 @@ fun LoginScreen(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.spacedBy(5.dp)
         ) {
+            IconButton(onClick = { navHostController.navigateUp() }) {
+                androidx.compose.material3.Icon(
+                    imageVector = Icons.Filled.ArrowBackIos,
+                    contentDescription = "Back to main screen",
+                    tint = androidx.compose.material.MaterialTheme.colors.onPrimary
+                )
+            }
             Text(
                 text = "Login",
                 fontSize = 55.sp,
